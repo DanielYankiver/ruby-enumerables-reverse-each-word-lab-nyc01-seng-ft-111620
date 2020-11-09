3f0f0d00f56297986b22343cde7055c4278b4ecd
Daniel Yankiver <dyankiver@gmail.com>
2020-11-09 19:14:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require 'pry'
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   sentence_array.each do |index|
-    index.reverse
+    index.reverse!
   end 
   sentence 
 end
